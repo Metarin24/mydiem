c5fa089ff9dbd450091a2f3dfcd48fa94ca6d1dc
Bùi Thanh Tâm <tambt2004@gmail.com>
2023-10-20 19:28:37 +0700

    New content4

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const nameGirl = "Diễm";
-const giftUrl = "https://www.facebook.com/mydiem.phan.121";
+const giftUrl = "https://www.facebook.com";
 const eventName = "Chúc 20/10 bé";
 const titleCard = "Tặng vợ tương lai :)";
 const contentCard ="Đừng buồn vì 20/10 mà chả có ai tặng quà, cả năm r tui cũng đã có ai tặng quà đâu haha. Thôi thì không ai tặng thì thôi để tui tặng 1 món quà tinh thần z ^^. Chúc bà 20/10 vui vẻ, mạnh khỏe, được nhiều bạn nam yêu quý và không bị deadline dí. Bà cũng nên quan tâm tới sức khỏe nhiều hơn vì tui thấy ngày nào bà cũng đi ngủ trễ ^^ . Tui chúc hơi công nghiệp xí :v";
